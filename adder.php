65b08dde7636c6abcde1e90740b0f889b8866666
diegofalconc <72233504+diegofalconc@users.noreply.github.com>
2020-10-22 09:21:14 -0700

    Update adder.php

--- adder.php
+++ adder.php
@@ -55,24 +55,6 @@
         }
     }
     ?>
-    <!--
-1)If statement should be placed below the form, so that it appears on the refresh in the right position
-2)$num2 is capped in the instance assignment of $myTotal, it should be $num2;
-3)The h2 closing tag is missing and the double and single quotation marks are mixed. 
-4)The <p> quotations most be changed to allow php variable input.
-5) There is a dot missing before $myTotal, also changing the double quotes to single quote identation around the variable
-6)Opening form tag has a misplaced slash
-7)one of the inputs have Num1 capped, it should be lowercase
-8)The third submit input is missing a double quotation in the value attribute
-9) the puntuation after the html closing tag most be deleted
-10) The label opening tags are missing and the closing ones are misplaced
-11) the method attribute was missing in the form tag
-12) one of the paragraphs break statement should be inside the paragraph, not outside
-13) Style should be in the headers, no need to have inline
-14) negative sign in the definition of myTotal most be removed
-15) missing href in the Reset Page
-16) Wrong symbols at the end of the html tag
--->
 </body>
 
 </html>
